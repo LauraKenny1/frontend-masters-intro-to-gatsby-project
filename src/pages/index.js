8088e31c25cc4588eea20fc0fb7b1924bfaa5df9
Laura Kenny <laura.kenny@cinch.co.uk>
2022-04-22 14:53:29 +0100

    Shared layout component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-import * as React from "react";
+import * as React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default function IndexPage() {
   return (
-    <main>
+    <Layout>
       <h1>Hello Frontend Masters!</h1>
-      <Link to="/about">About</Link>
-    </main>
+      <Link to="/about">About this site</Link>
+    </Layout>
   );
 }
